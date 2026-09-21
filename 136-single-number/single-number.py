8c0432a78304e2c765288967c4b878400fe78896
Challabathula Veera Venkata Manikiran <manikiranchallabathula@gmail.com>
2026-09-21 15:33:37 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (12.55%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        freq = {}
+        for i in nums:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] =1
+            
+        for i in nums:
+            if freq[i] > 1:
+                pass
+            else:
+                return i
+        return None
